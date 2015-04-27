38839144699ff1c2b6d510cc55dff579b15a359b
elektrowolle <internetbewohner@gmail.com>
2015-04-28 01:27:47 +0200

    Converting to works

--- Braile.js
+++ Braile.js
@@ -1,5 +1,6 @@
+var endOfLine = '\r\n';
 Braile = {};
-
+Braile.en = {};
 Braile.en.alphabet = {
     "a"   : "o- -- --",
     "b"   : "o- o- --",
@@ -57,33 +58,40 @@ Braile.convertToBraile = function(str, lng){
     line[1]    = "";
     line[2]    = "";
 
-    var braile = Braile[lng ? lng : "en"];
+    var braile = Braile[lng ? lng : "en"].alphabet;
     for (var i = str.length - 1; i >= 0; i--) {
-        var character = braile[str[i]];
+        var character = str[i];
         if(character){
-            var characterParts = character.split(character, ' ');
+            var characterParts = braile[character].split(' ');
             line[0] = line[0].concat(characterParts[0]);
             line[1] = line[1].concat(characterParts[1]);
             line[2] = line[2].concat(characterParts[2]);
         }
     };
+    var returnStr =
+        (line[0] + endOfLine +
+        line[1] + endOfLine +
+        line[2]);
+
+    return returnStr;
 }
 
 Braile.convertFromBraile = function(str, lng){
     var returnStr = '';
-    var newstr = str.split(str, '/n');
-    var braile = Braile[lng ? lng : "en"];
+    var newstr = str.split(str, endOfLine);
+    var braile = Braile[lng ? lng : "en"].alphabet;
     for (var i = 1; i < newstr[0].length; i += 2) {
         for (var _iBraile in braile) {
-            if( (newstr[0][i] + ' '  + newstr[1][i] + ' ' + newstr[2][i]) 
+            if( (newstr[0][i] + ' '  + newstr[1][i] + ' ' + newstr[2][i])
                 == braile[_iBraile]){
 
                 returnStr = returnStr.concat(braile);
             }
 
         };
-        
+
     };
+    return returnStr;
 }
 
 module.export = Braile;
